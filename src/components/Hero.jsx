4b7f93b427747fcb19979be90a992ba86e7bdccf
hanna <bykovskaya.hanna@gmail.com>
2025-01-30 15:45:26 +0300

    mobile version

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,11 @@ import { HERO_CONTENT } from "../constants"
 
 const Hero = () => {
 	return (
-		<section className="max-w-7xl mx-auto border-b-2">
+		<section className="max-w-7xl mx-auto border-b-2 px-2">
 			<div className="flex flex-col items-center my-20">
-				<h1 className="text-6xl lg:text-[10rem]
+				<h1 className="text-2xl lg:text-[10rem]
+				md:text-6xl sm:text-6xl
+
 				 p-2 uppercase font-bold">
 					{HERO_CONTENT.title}
 				</h1>
@@ -14,7 +16,8 @@ const Hero = () => {
 					{HERO_CONTENT.subtitle}
 				</p>
 				<img src={HERO_CONTENT.image}
-					className="h-[20vh]  w-full lg:h-[65vh] md:h-[65vh]
+					className="h-[30vh]  w-full lg:h-[65vh] md:h-[65vh]
+					sm:h-[45vh]
 					object-cover rounded-2xl p-2"
 					alt="RelaxSpace"/>
 			</div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,37 +4,36 @@ import { RiCloseLine, RiMenu3Line } from "@remixicon/react"
 import {LINKS} from "../constants"
 
 const Navbar = () => {
-	const [isOpen, setIsOpen] = useState(false)
+	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleMenu = () => {
-		setIsOpen(!isOpen)
-	}
-
+		setIsOpen(!isOpen);
+	};
 
 	return (
-		<nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center  py-3 
-		border-b-2 bg-white">
-		<div className="container px-4 mx-auto relative text-sm">
-
-				<div className="flex justify-between items-center"
-				>
-					<a href="/" className="flex items-center flex-shrink-0"
+		<nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center py-3 border-b-2 bg-white">
+			<div className="w-full relative text-sm">
+				<div className="flex justify-between items-center px-4">
+					<a
+						href="/"
+						className="flex items-center flex-shrink-0 uppercase text-xl font-bold hover:text-neutral-700"
 						onClick={(e) => {
-						e.preventDefault();
-						window.scrollTo({
-						top: 0,
-						behavior: "smooth",
-						});
-					}}
-						
-						>
-						<img src={logo} width={150} height={15} alt="RelaxSpace" />
-
+							e.preventDefault();
+							window.scrollTo({
+								top: 0,
+								behavior: "smooth",
+							});
+						}}
+					>
+						RelaxSpace
 					</a>
-				<ul className="hidden lg:flex ml-14 space-x-12">
+					<ul className="hidden lg:flex ml-14 space-x-12">
 						{LINKS.map((link, index) => (
-							<li key={index} >
-								<a href={link.link} className="uppercase text-sm font-medium hover:text-neutral-600">
+							<li key={index}>
+								<a
+									href={link.link}
+									className="uppercase text-sm font-medium hover:text-neutral-600"
+								>
 									{link.name}
 								</a>
 							</li>
@@ -46,31 +45,31 @@ const Navbar = () => {
 							{isOpen ? <RiCloseLine /> : <RiMenu3Line />}
 						</button>
 					</div>
-
 				</div>
-				
 
 				{isOpen && (
-						<div 					 
-							  className="fixed right-0 z-20 bg-white
-							w-full p-12 flex flex-col justify-center items-start lg:hidden">
-							<ul className="">
-								{LINKS.map((link, index) => (
-									<li key={index} className="py-4">
-										<a href={link.link} className="uppercase hover:text-neutral-600">
-											{link.name}
-										</a>
-									</li>
-								)
-								)}
-							</ul>
-							
-						</div>
-						)
-					}
-				</div>
+					<div
+						className="fixed right-0 
+						z-20 bg-white w-full py-12 px-4 flex flex-col justify-center items-start lg:hidden"
+					>
+						<ul>
+							{LINKS.map((link, index) => (
+								<li key={index} className="py-4">
+									<a
+										href={link.link}
+										className="uppercase hover:text-neutral-600"
+									>
+										{link.name}
+									</a>
+								</li>
+							))}
+						</ul>
+					</div>
+				)}
+			</div>
 		</nav>
-	)
-}
+	);
+};
+
 
 export default Navbar
